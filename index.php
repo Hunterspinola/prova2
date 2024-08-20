78facdbe5dcebfd4d47d1aaa9277f989b0abe789
gigi <giovanah.ps@gmail.com>
2024-08-20 10:14:23 -0300

    sim

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset>
+        <legend>MENU</legend>
+    <ul class="menu">
+        <li><a href="Cadastro_fluxo_caixa.html">Cadastro fluxo de caixa</a></li>
+        <li><a href="listar_fluxo_caixa.php">Listagem de fluxo de caixa</a></li>
+        <li><a href="consulta_fluxo_caixa.html">Consulta fluxo do caixa</a></li>
+    </ul>
+    </fieldset>
+</body>
+</html>
